0679516bc1b2c792512a548ab1b76e4fa1e2e8e3
David RD <pepoitdog@gmail.com>
2022-09-20 10:27:28 +0200

    v0.2

--- deploy.sh
+++ deploy.sh
@@ -143,8 +143,8 @@ sudo mv kubectl kustomize calicoctl oc /usr/local/bin
 
 
 # Configure zsh
-#sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 #Set ZSH_THEME="powerlevel10k/powerlevel10k" in ~/.zshrc.
 
 # List Versions
